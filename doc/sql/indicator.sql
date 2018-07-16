5617460728a8917c8efdba5561c6691682c8c46f
xuHaiLiang5794 <2764243724@qq.com>
2018-07-16 11:33:59 +0800

    添加E-R模型

--- doc/sql/indicator.sql
+++ doc/sql/indicator.sql
@@ -1,24 +1,24 @@
 
-CREATE TABLE t_expression
+CREATE TABLE t_ind_expression
 (
 	ID                   CHAR(18) NOT NULL,
 	expr_code            CHAR(18) NOT NULL
 );
 
-ALTER TABLE t_expression
+ALTER TABLE t_ind_expression
 ADD PRIMARY KEY (ID);
 
-CREATE TABLE t_expression_code
+CREATE TABLE t_ind_expression_code
 (
 	ID                   CHAR(18) NOT NULL,
 	code                 INTEGER NULL,
 	remark               VARCHAR(20) NULL
 );
 
-ALTER TABLE t_expression_code
+ALTER TABLE t_ind_expression_code
 ADD PRIMARY KEY (ID);
 
-CREATE TABLE t_indicator_classification
+CREATE TABLE t_ind_indicator_classification
 (
 	ID                   CHAR(18) NOT NULL,
 	clazz                VARCHAR(20) NULL,
@@ -26,10 +26,10 @@ CREATE TABLE t_indicator_classification
 	data_time            DATE NULL
 );
 
-ALTER TABLE t_indicator_classification
+ALTER TABLE t_ind_indicator_classification
 ADD PRIMARY KEY (ID);
 
-CREATE TABLE t_indicator_library
+CREATE TABLE t_ind_indicator_library
 (
 	ID                   CHAR(18) NOT NULL,
 	fun_name             VARCHAR(20) NULL,
@@ -39,10 +39,10 @@ CREATE TABLE t_indicator_library
 	where_id             CHAR(18) NOT NULL
 );
 
-ALTER TABLE t_indicator_library
+ALTER TABLE t_ind_indicator_library
 ADD PRIMARY KEY (ID);
 
-CREATE TABLE t_indicator_where
+CREATE TABLE t_ind_indicator_where
 (
 	ID                   CHAR(18) NOT NULL,
 	value                VARCHAR(20) NULL,
@@ -51,29 +51,29 @@ CREATE TABLE t_indicator_where
 	where_code           INTEGER NOT NULL
 );
 
-ALTER TABLE t_indicator_where
+ALTER TABLE t_ind_indicator_where
 ADD PRIMARY KEY (ID);
 
-CREATE TABLE t_where_code
+CREATE TABLE t_ind_where_code
 (
 	code                 INTEGER NOT NULL,
 	remark               VARCHAR(20) NULL
 );
 
-ALTER TABLE t_where_code
+ALTER TABLE t_ind_where_code
 ADD PRIMARY KEY (code);
 
-ALTER TABLE t_expression
-ADD FOREIGN KEY R_26 (expr_code) REFERENCES t_expression_code (ID);
+ALTER TABLE t_ind_expression
+ADD FOREIGN KEY R_26 (expr_code) REFERENCES t_ind_expression_code (ID);
 
-ALTER TABLE t_indicator_library
-ADD FOREIGN KEY R_27 (clazz_id) REFERENCES t_indicator_classification (ID);
+ALTER TABLE t_ind_indicator_library
+ADD FOREIGN KEY R_27 (clazz_id) REFERENCES t_ind_indicator_classification (ID);
 
-ALTER TABLE t_indicator_library
-ADD FOREIGN KEY R_28 (where_id) REFERENCES t_indicator_where (ID);
+ALTER TABLE t_ind_indicator_library
+ADD FOREIGN KEY R_28 (where_id) REFERENCES t_ind_indicator_where (ID);
 
-ALTER TABLE t_indicator_where
-ADD FOREIGN KEY R_22 (where_code) REFERENCES t_where_code (code);
+ALTER TABLE t_ind_indicator_where
+ADD FOREIGN KEY R_22 (where_code) REFERENCES t_ind_where_code (code);
 
-ALTER TABLE t_indicator_where
-ADD FOREIGN KEY R_25 (expr_id) REFERENCES t_expression (ID);
+ALTER TABLE t_ind_indicator_where
+ADD FOREIGN KEY R_25 (expr_id) REFERENCES t_ind_expression (ID);
