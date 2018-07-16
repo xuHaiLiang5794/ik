0a97d7eb4e7303128fdf6c8bdcc5c41c165a36a9
xuHaiLiang5794 <2764243724@qq.com>
2018-07-16 11:20:38 +0800

    添加E-R模型

---
+++ doc/sql/indicator.sql
@@ -0,0 +1,79 @@
+
+CREATE TABLE t_expression
+(
+	ID                   CHAR(18) NOT NULL,
+	expr_code            CHAR(18) NOT NULL
+);
+
+ALTER TABLE t_expression
+ADD PRIMARY KEY (ID);
+
+CREATE TABLE t_expression_code
+(
+	ID                   CHAR(18) NOT NULL,
+	code                 INTEGER NULL,
+	remark               VARCHAR(20) NULL
+);
+
+ALTER TABLE t_expression_code
+ADD PRIMARY KEY (ID);
+
+CREATE TABLE t_indicator_classification
+(
+	ID                   CHAR(18) NOT NULL,
+	clazz                VARCHAR(20) NULL,
+	remark               VARCHAR(20) NULL,
+	data_time            DATE NULL
+);
+
+ALTER TABLE t_indicator_classification
+ADD PRIMARY KEY (ID);
+
+CREATE TABLE t_indicator_library
+(
+	ID                   CHAR(18) NOT NULL,
+	fun_name             VARCHAR(20) NULL,
+	fun_expr             VARCHAR(20) NULL,
+	data_time            DATE NULL,
+	clazz_id             CHAR(18) NOT NULL,
+	where_id             CHAR(18) NOT NULL
+);
+
+ALTER TABLE t_indicator_library
+ADD PRIMARY KEY (ID);
+
+CREATE TABLE t_indicator_where
+(
+	ID                   CHAR(18) NOT NULL,
+	value                VARCHAR(20) NULL,
+	expr_id              VARCHAR(20) NOT NULL,
+	priority             INTEGER NULL,
+	where_code           INTEGER NOT NULL
+);
+
+ALTER TABLE t_indicator_where
+ADD PRIMARY KEY (ID);
+
+CREATE TABLE t_where_code
+(
+	code                 INTEGER NOT NULL,
+	remark               VARCHAR(20) NULL
+);
+
+ALTER TABLE t_where_code
+ADD PRIMARY KEY (code);
+
+ALTER TABLE t_expression
+ADD FOREIGN KEY R_26 (expr_code) REFERENCES t_expression_code (ID);
+
+ALTER TABLE t_indicator_library
+ADD FOREIGN KEY R_27 (clazz_id) REFERENCES t_indicator_classification (ID);
+
+ALTER TABLE t_indicator_library
+ADD FOREIGN KEY R_28 (where_id) REFERENCES t_indicator_where (ID);
+
+ALTER TABLE t_indicator_where
+ADD FOREIGN KEY R_22 (where_code) REFERENCES t_where_code (code);
+
+ALTER TABLE t_indicator_where
+ADD FOREIGN KEY R_25 (expr_id) REFERENCES t_expression (ID);
---
+++ doc/sql/tpAccount.sql
@@ -0,0 +1,28 @@
+
+CREATE TABLE T_TP_PARTNER
+(
+	ID                   VARCHAR(20) NOT NULL,
+	partner_name         VARCHAR(20) NULL,
+	partner_begin_time   DATE NULL,
+	partner_end_time     DATE NULL,
+	data_time            DATE NULL,
+	remark               VARCHAR(20) NULL
+);
+
+ALTER TABLE T_TP_PARTNER
+ADD PRIMARY KEY (ID);
+
+CREATE TABLE T_TP_SECRET_KEY
+(
+	secret_key           VARCHAR(20) NULL,
+	value                VARCHAR(20) NULL,
+	ID                   CHAR(18) NOT NULL,
+	remark               VARCHAR(20) NULL,
+	partner_id           VARCHAR(20) NULL
+);
+
+ALTER TABLE T_TP_SECRET_KEY
+ADD PRIMARY KEY (ID);
+
+ALTER TABLE T_TP_SECRET_KEY
+ADD FOREIGN KEY R_19 (partner_id) REFERENCES T_TP_PARTNER (ID);
